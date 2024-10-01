ae5b405fcaaaf23476163b8ac1dea952177f833a
Grigory56 <95852762+Grigory56@users.noreply.github.com>
2024-10-02 06:40:43 +0700

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,7 +1,10 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
+
 print_params( b = 25 )
 print_params( c = [1,2,3] )
+print_params( b = 18,a='fist' )
+print_params()
 values_list=[5,'test',False]
 values_dict={'a':25, 'b': 'dictionary','c':0.25}
 print_params(*values_list)
